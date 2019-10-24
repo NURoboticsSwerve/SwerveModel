1b8b8ae4c5cb5b9c51fc1d42f94a3007b95dcc41
Ben Fox <b2k16@cinderhippo.com>
2019-10-23 21:13:15 -0400

    Set up wheel class

--- SwerveMath.py
+++ SwerveMath.py
@@ -1,1 +1,19 @@
 # Put math here
+
+
+class Wheel:
+
+    def __init__(self):
+        self.ang = 0
+        self.vel = 0
+        self.rot_vel = 0
+
+    # x, y, and rot are all -1 to 1 values
+    def set_motion(self, x, y, rot):
+        print("Do nothing")
+
+
+top_left = Wheel()
+top_right = Wheel()
+back_left = Wheel()
+back_right = Wheel()
